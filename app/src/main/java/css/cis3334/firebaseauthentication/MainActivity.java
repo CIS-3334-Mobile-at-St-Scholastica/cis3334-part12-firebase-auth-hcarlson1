c80e70ac1c7ada48a0c60ec9b55c5f426d568cfa
Heather <hcarlson1@css.edu>
2017-04-13 13:30:05 -0500

    Add detailed comments, removed Log messages and reaplaced with comments, and removed Toast messges which were replaced with set texts for text view.

--- app/src/main/java/css/cis3334/firebaseauthentication/MainActivity.java
+++ app/src/main/java/css/cis3334/firebaseauthentication/MainActivity.java
@@ -26,6 +26,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewStatus;
@@ -36,9 +37,16 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonCreateLogin;
     private Button buttonSignOut;
 
-    private FirebaseAuth mAuth; //for authorization
-    private FirebaseAuth.AuthStateListener mAuthListener; //authorization listener
+    private FirebaseAuth mAuth; //Firebas authorization object
+    private FirebaseAuth.AuthStateListener mAuthListener; //Firebas authorization listener object
 
+    /**
+     * onCreate method creates everything needed for the app to run. Specifically to Firebase a couple
+     * of instances needed to be initialized such as of the FirebaseAuth instance and AuthStateListener method
+     * so tracking of when a user signs in or out can occur.
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,14 +62,14 @@ public class MainActivity extends AppCompatActivity {
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d("CIS3334", "normal login ");
+                //old log that said it was an ormal log in
                 signIn(editTextEmail.getText().toString(), editTextPassword.getText().toString());
             }
         });
 
         buttonCreateLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d("CIS3334", "Create Account ");
+                //old log saying the account has been created
                 createAccount(editTextEmail.getText().toString(), editTextPassword.getText().toString());
             }
         });
@@ -75,89 +83,139 @@ public class MainActivity extends AppCompatActivity {
 
         buttonSignOut.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d("CIS3334", "Logging out - signOut ");
+                //old log saying signed out
                 signOut();
             }
         });
 
-        mAuth = FirebaseAuth.getInstance(); //initilizezed
+        mAuth = FirebaseAuth.getInstance(); //Initialize FirebaseAuth instance so it can track when a user signs in or out
 
-        mAuthListener = new FirebaseAuth.AuthStateListener() { //authorization listener
+        mAuthListener = new FirebaseAuth.AuthStateListener() { //Initialized AuthStateListener method so it can track when a user signs in or out
+
+            /**
+             * onAuthStateChanged method will create a Firebase user object so it caan be used to check if
+             * there is an user signed in. The if statement is what actually checks is a user is signed in
+             * by simply checking if there is a user or not when firebaseAuth.getCurrentUser is called. If it
+             * returns null then the user is signed out and if it is not null then a user is signed in.
+             *
+             * @param firebaseAuth
+             */
+
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    // User is signed in
-                    Log.d("CIS3334", "onAuthStateChanged:signed_in:" + user.getUid());
+                    // Old log call that logged a User is signed in along with the Users ID
+
                 } else {
-                    // User is signed out
-                    Log.d("CIS3334", "onAuthStateChanged:signed_out");
+                    // Old log call that there isn't a user signed in
+
                 }
-                // ...
+
             }
         };
     }
 
+    /**
+     * onStart is attaching the listener for the authorization on this method.
+     */
     @Override
-    public void onStart() { //sets up the listner
+    public void onStart() { //method header of no type and returns void
         super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
+        mAuth.addAuthStateListener(mAuthListener); //FirebaseAuth object to access addAuthStateListener method
+                                                   // to add a authorization state listener using FirebaseAuth.AuthStateListener
+                                                   // as the parameter for the method call
     }
 
+    /**
+     * onStop removes the attached listener for the authorization
+     */
     @Override
-    public void onStop() { //stops the listener
+    public void onStop() { //method header of no type and returns void
         super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
+        if (mAuthListener != null) { //As long as  FirebaseAuth.AuthStateListener object is not null...
+            mAuth.removeAuthStateListener(mAuthListener); //FirebaseAuth object to access removeAuthStateListener method
+                                                          // to remove a authorization state listener using FirebaseAuth.AuthStateListener
+                                                          // as the parameter for the method call
         }
     }
 
+    /**
+     * createAccount method creates an user account on the FireBase by putting in the users email and password in the database
+     *
+     * @param email
+     * @param password
+     */
     private void createAccount(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        mAuth.createUserWithEmailAndPassword(email, password) //FirebaseAuth object to access createUserWithEmailAndPassword method
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() { //FirebaseAuth object to access addOnCompleteListener method
+
+                    /**
+                     * onComplete method sets the Text View object to inform the account was created successfully.
+                     * Otherwise in the If statement if the task failed then it would display a toast message saying so.
+                     * Updated it now displays the authentication failed in the text view.
+                     *
+                     * @param task
+                     */
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d("CIS3334", "createUserWithEmail:onComplete:" + task.isSuccessful());
-                        textViewStatus.setText("Status: User Created!");
+                        //Old log that logged that the task was successful
+                        textViewStatus.setText("Status: User Created!"); //Updated the Text View to let user know the account has been created
 
                         // If sign in fails, display a message to the user. If sign in succeeds
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            textViewStatus.setText("Authentication failed."); //Updated the Text View to let user know Authentication Failed
+                            //old toast message here
                         }
 
-                        // ...
+
                     }
                 });
     }
 
+    /**
+     * signIn method signs the user in by connecting to the database to make sure the credential match with what is in the database.
+     *
+     * @param email
+     * @param password
+     */
     private void signIn(String email, String password){
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        mAuth.signInWithEmailAndPassword(email, password) //FirebaseAuth object to access signInWithEmailAndPassword method
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() { //FirebaseAuth object to access addOnCompleteListener method
+
+                    /**
+                     * onComplete method sets the Text View object to inform the account was signed in successfully.
+                     * Otherwise in the If statement if the task failed then it would display a toast message saying so.
+                     * Updated it now displays the authentication failed in the text view.
+                     * @param task
+                     */
+
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d("CIS3334", "signInWithEmail:onComplete:" + task.isSuccessful());
-                        textViewStatus.setText("Status: Signed In!");
+                        //old log message saying the sign in was successful
+                        textViewStatus.setText("Status: Signed In!"); //Updated the Text View to let user know they are signed in
 
                         // If sign in fails, display a message to the user. If sign in succeeds
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
-                            Log.w("CIS3334", "signInWithEmail", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            //Old log message saying there was an exception
+                            textViewStatus.setText("Authentication failed."); //Updated the Text View to let user know Authentication Failed
+                            //old toast message here
                         }
-
-                        // ...
                     }
                 });
     }
 
+    /**
+     * signOut simply signs the user out by using the FirebaseAuth object to access signOut method.
+     * It also changes the text view to updat let the user know they have been signed out.
+     */
     private void signOut () {
-        textViewStatus.setText("Status: Signed Out!");
-        mAuth.signOut();
+        textViewStatus.setText("Status: Signed Out!"); //Updated the Text View to let user know they are signed out
+        mAuth.signOut(); //FirebaseAuth object to access signOut method
     }
 
     private void googleSignIn() {
